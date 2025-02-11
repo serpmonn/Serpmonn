be61e1bb85598ff7ff722899dc0e90fdf8f3da28
Sergei <serpmon@gmail.com>
2025-02-11 06:34:04 +0300

    Добавление кнопки перейти к входу в SerpmonnConfirmBot

--- telegram_bots/SerpmonnConfirmBot/SerpmonnConfirmBot.mjs
+++ telegram_bots/SerpmonnConfirmBot/SerpmonnConfirmBot.mjs
@@ -1,16 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: '/var/www/serpmonn.ru/.env' });
 
-import { query } from '../../backend/database/config.mjs';  							// Импортируем функцию query для выполнения SQL-запросов
+import { query } from '../../backend/database/config.mjs';                                                      		// Импортируем функцию query для выполнения SQL-запросов
 import TelegramBot from 'node-telegram-bot-api';
-import fs from 'fs';
+import fs from 'fs/promises';
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
 // Функция для получения временных данных регистрации
-function getTempRegistrationData(userId) {
+async function getTempRegistrationData(userId) {
     try {
-        const tempData = JSON.parse(fs.readFileSync('/var/www/serpmonn.ru/backend/auth/tempUserData.json', 'utf-8') || '{}');
+        const tempData = JSON.parse(await fs.readFile('/var/www/serpmonn.ru/backend/auth/tempUserData.json', 'utf-8') || '{}');
         return tempData[userId];
     } catch (error) {
         console.error('Ошибка при получении временных данных:', error);
@@ -19,11 +19,11 @@ function getTempRegistrationData(userId) {
 }
 
 // Функция для удаления временных данных после завершения регистрации
-function deleteTempRegistrationData(userId) {
+async function deleteTempRegistrationData(userId) {
     try {
-        const tempData = JSON.parse(fs.readFileSync('/var/www/serpmonn.ru/backend/auth/tempUserData.json', 'utf-8') || '{}');
+        const tempData = JSON.parse(await fs.readFile('/var/www/serpmonn.ru/backend/auth/tempUserData.json', 'utf-8') || '{}');
         delete tempData[userId];
-        fs.writeFileSync('tempUserData.json', JSON.stringify(tempData));
+        await fs.writeFile('/var/www/serpmonn.ru/backend/auth/tempUserData.json', JSON.stringify(tempData));
     } catch (error) {
         console.error('Ошибка при удалении временных данных:', error);
     }
@@ -34,29 +34,38 @@ bot.on('message', async (msg) => {
     const text = msg.text;
 
     if (text.startsWith('/start ')) {
-        const userId = text.split(' ')[1]; 									// Получаем userId из диплинка
-	console.log("User ID:", userId);  									// Логируем userId
-        // Проверяем, нет ли уже этого пользователя в БД
-        const [existingUser] = await query("SELECT id FROM users WHERE id = ?", [userId]);
+        const userId = text.split(' ')[1];                                                                      		// Получаем userId из диплинка
+        console.log("User ID:", userId);                                                                        		// Логируем userId
 
-        if (existingUser && existingUser.length > 0) {
+        const [existingUser] = await query("SELECT id FROM users WHERE id = ?", [userId]);					// Проверяем, нет ли уже этого пользователя в БД
+	
+	if (existingUser) {
             bot.sendMessage(chatId, "Вы уже зарегистрированы!");
             return;
         }
 
-        const tempData = await getTempRegistrationData(userId); 						// Тут можно хранить временные данные о регистрации (например, email, пароль)
+        const tempData = await getTempRegistrationData(userId);                                                 		// Тут можно хранить временные данные о регистрации (например, email, пароль)
 
         if (!tempData) {
             bot.sendMessage(chatId, "Ошибка: регистрационные данные не найдены.");
             return;
         }
 
-        // Сохраняем в БД
-        await query("INSERT INTO users (id, username, email, password_hash, confirmed) VALUES (?, ?, ?, ?, ?)", 
+        await query("INSERT INTO users (id, username, email, password_hash, confirmed) VALUES (?, ?, ?, ?, ?)",	 		// Сохраняем в БД
             [userId, tempData.username, tempData.email, tempData.passwordHash, true]);
 
-        deleteTempRegistrationData(userId);									// Удаляем временные данные после успешной регистрации
+        deleteTempRegistrationData(userId);                                                                     		// Удаляем временные данные после успешной регистрации
 
-        bot.sendMessage(chatId, "Вы успешно подтвердили регистрацию! Теперь можете войти в аккаунт.");
+        const loginButton = {													// Создаем кнопку с URL, ведущую на страницу логина
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: 'Перейти к входу', url: 'https://serpmonn.ru/frontend/login/login.html' }  			// Ссылка на страницу логина
+                    ]
+                ]
+            }
+        };
+
+        bot.sendMessage(chatId, "Вы успешно подтвердили регистрацию! Теперь можете войти в аккаунт.", loginButton);
     }
 });
