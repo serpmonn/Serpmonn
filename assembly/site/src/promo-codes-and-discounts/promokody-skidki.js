37a00e45183ebcf9db287e7568b7d463d4252d21
Sergei <sergei@serpmonn.ru>
2026-06-21 15:53:26 +0300

    feat: remove X and WhatsApp, add Email share in popup

--- assembly/site/src/promo-codes-and-discounts/promokody-skidki.js
+++ assembly/site/src/promo-codes-and-discounts/promokody-skidki.js
@@ -1213,23 +1213,17 @@ function getOrCreateShareOverlay() {
                     </span>
                     Telegram
                 </a>
-                <a class="share-network-btn" id="sn-wa" href="#" target="_blank" rel="noopener noreferrer" aria-label="WhatsApp">
-                    <span class="sn-icon sn-icon--wa">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413z"/></svg>
-                    </span>
-                    WhatsApp
-                </a>
-                <a class="share-network-btn" id="sn-x" href="#" target="_blank" rel="noopener noreferrer" aria-label="X (Twitter)">
-                    <span class="sn-icon sn-icon--x">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.744l7.737-8.835L1.254 2.25H8.08l4.261 5.636L18.244 2.25zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>
-                    </span>
-                    X
-                </a>
                 <a class="share-network-btn" id="sn-ok" href="#" target="_blank" rel="noopener noreferrer" aria-label="Одноклассники">
                     <span class="sn-icon sn-icon--ok">
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11S18.075 1 12 1zm0 4.5a3.5 3.5 0 110 7 3.5 3.5 0 010-7zm3.75 9.25c.47.47.47 1.23 0 1.7l-1.9 1.9c.52.14 1.05.23 1.6.27.66.05 1.16.62 1.11 1.28-.05.66-.62 1.16-1.28 1.11-1.45-.11-2.82-.62-3.98-1.44-1.16.82-2.53 1.33-3.98 1.44-.66.05-1.23-.45-1.28-1.11-.05-.66.45-1.23 1.11-1.28.55-.04 1.08-.13 1.6-.27l-1.9-1.9c-.47-.47-.47-1.23 0-1.7.47-.47 1.23-.47 1.7 0L12 16.54l2.05-2.04c.47-.47 1.23-.47 1.7.25z"/></svg>
                     </span>
                     ОК
+                </a>
+                <a class="share-network-btn" id="sn-email" href="#" aria-label="Email">
+                    <span class="sn-icon sn-icon--email">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/></svg>
+                    </span>
+                    Email
                 </a>
             </div>
             <div class="share-popup__url-row">
@@ -1287,12 +1281,10 @@ function openSharePopup(promo, url) {
         `https://vk.com/share.php?url=${enc(url)}&title=${enc(shareText)}`;
     overlay.querySelector('#sn-tg').href =
         `https://t.me/share/url?url=${enc(url)}&text=${enc(shareText)}`;
-    overlay.querySelector('#sn-wa').href =
-        `https://api.whatsapp.com/send?text=${enc(shareText + ' ' + url)}`;
-    overlay.querySelector('#sn-x').href =
-        `https://twitter.com/intent/tweet?url=${enc(url)}&text=${enc(shareText)}`;
     overlay.querySelector('#sn-ok').href =
         `https://connect.ok.ru/offer?url=${enc(url)}&title=${enc(shareText)}`;
+    overlay.querySelector('#sn-email').href =
+        `mailto:?subject=${enc('Промокод от Serpmonn')}&body=${enc(shareText + '\n\n' + url)}`;
 
     overlay.classList.add('visible');
     overlay.querySelector('.share-popup__close').focus();
